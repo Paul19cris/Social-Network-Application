d73e053aba31e28208260ce23192f270567b9932
Paul19cris <97522288+Paul19cris@users.noreply.github.com>
2023-03-28 18:16:55 +0300

    Delete frontend directory

--- frontend/src/components/Appbar/services.js
+++
@@ -1,7 +0,0 @@
-import { axiosInstance } from "../../api/api"
-
-export const callAllUsersByKey = async (key) => axiosInstance.get(`/account/getUsersByKey?key=${key}`)
-
-export const callAllUsers = async () => axiosInstance.get(`/account/getUsers`)
-
-export const getUnseenNrCall = async (username) => axiosInstance.get(`/account/getUnseenNotifications?username=${username}`)
